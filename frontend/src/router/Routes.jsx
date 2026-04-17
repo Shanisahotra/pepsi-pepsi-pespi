128de7ca0cca01a09151f3aa68f2d5425cc0a10e
Shanisahotra <jonathanzeeshan25@gmail.com>
2026-04-17 21:10:57 +0500

    private-comp

--- frontend/src/router/Routes.jsx
+++ frontend/src/router/Routes.jsx
@@ -1,12 +1,23 @@
 import { Routes, Route } from "react-router-dom"
 import { LoginForm } from "@/components/ui/form"
 import Layout from "@/component/layout"
+import PrivateRoute from "./PrivateRoute"
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<LoginForm />} />
-      <Route path="/layout" element={<Layout />} />
+      {/* <Route path="/layout" element={<Layout />} /> */}
+
+         {/* Private Route */}
+      <Route
+        path="/layout"
+        element={
+          <PrivateRoute>
+            <Layout />
+          </PrivateRoute>
+        }
+      />
     </Routes>
   )
 }
